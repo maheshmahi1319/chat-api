288a766458f0427ae34a29ac743d474f5b90103c
Mahesh Kasodhan <kasaudhan.mahesh021@gmail.com>
2020-12-13 21:19:51 +0530

    done

--- server/routes/chatRoomOneToOne.js
+++ server/routes/chatRoomOneToOne.js
@@ -4,12 +4,6 @@ import chatRoomOneToOne from "../controllers/chatOneToOne.js";
 
 const router = express.Router();
 
-// router.get("/", chatRoom.getRecentConversation);
-// router.get("/:roomId", chatRoom.getConversationByRoomId);
-// router.post("/initiate", chatRoom.initiate);
-// router.post("/:roomId/message", chatRoom.postMessage);
 router.post("/:roomId/messageonetoone", chatRoomOneToOne.postMessageToUser);
-// router.post("/:userId/message", chatRoom.postMessageToUser);
-// router.put("/:roomId/mark-read", chatRoom.markConversationReadByRoomId);
 
 export default router;
